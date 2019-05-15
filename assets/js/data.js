e6b536a7fe50e1575079abfc0c162680afce1a37
Vipin Yadav <imvpn22@gmail.com>
2019-05-15 21:41:30 +0530

    added dynamic code

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,17 +2,41 @@ let boards = [{
     listTitle: "Pending",
     tasks: [{
         priority: "low",
-        title: "Complete this JSON data",
+        description: "Complete this JSON data",
         commentCount: 3,
         linkCount: 4,
     }, {
         priority: "medium",
-        title: "What are you doing",
+        description: "What are you doing",
         commentCount: 2,
         linkCount: 2,
     }, {
         priority: "high",
-        title: "This is very high priority task",
+        description: "This is very high priority task",
+        commentCount: 5,
+        linkCount: 10,
+    }, {
+        priority: "medium",
+        description: "So now what",
+        commentCount: 2,
+        linkCount: 3,
+    }]
+
+}, {
+    listTitle: "In Progress",
+    tasks: [{
+        priority: "low",
+        description: "UI Design",
+        commentCount: 3,
+        linkCount: 4,
+    }, {
+        priority: "medium",
+        description: "What are you doing",
+        commentCount: 2,
+        linkCount: 2,
+    }, {
+        priority: "high",
+        description: "This is very high priority task",
         commentCount: 5,
         linkCount: 10,
     }]
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,125 @@
-console.log(boards);
+// console.log(boards);
 
 
-const boardContainer = document.getElementById('boardContainer');
+const createCards = ( taskList ) => {
+    const boardContainer = document.querySelector('#boardContainer');
 
-console.log(boardContainer);
+    taskList.forEach( list => {
+        console.log(list);
+        let taskListCard = document.createElement('div');
+        taskListCard.className = 'task-list-card';
+
+        let taskListTitle = document.createElement('div');
+        taskListTitle.className = 'task-list-title';
+        taskListTitle.innerHTML = list.listTitle;
+
+        taskListCard.appendChild(taskListTitle);
+
+        createTask(list.tasks, taskListCard);
+
+        boardContainer.appendChild(taskListCard);
+    });
+
+}
+
+const createTask = (tasks, taskListCard) => {
+    tasks.forEach(task => {
+        console.log(task);
+
+        const taskCard = document.createElement('div');
+        taskCard.className = 'task-card';
+
+        const taskPriority = document.createElement('div');
+        taskPriority.className = 'task-priority';
+        taskPriority.classList.add(task.priority === 'high' ? 'high-priority' :
+            task.priority === 'medium' ? 'med-priority' : 'low-priority');
+        taskPriority.innerHTML = task.priority;
+        taskCard.appendChild(taskPriority);
+
+        const taskDescription = document.createElement('div');
+        taskDescription.className = 'task-description';
+        taskDescription.innerHTML =  task.description;
+        taskCard.appendChild(taskDescription);
+
+        const taskFooter = document.createElement('div');
+        taskFooter.className = 'task-footer';
+
+        const taskComment = document.createElement('div');
+        taskComment.className = 'task-comment';
+
+        const commentIcon = document.createElement('i');
+        commentIcon.classList.add('far', 'fa-comment-dots');
+
+        let commentCount = document.createElement('span');
+        commentCount.innerHTML = task.commentCount;
+
+        taskComment.appendChild(commentIcon);
+        taskComment.appendChild(commentCount);
+
+        taskFooter.appendChild(taskComment);
+
+        // Task Link
+        const taskLink = document.createElement('div');
+        taskLink.className = 'task-link';
+
+        const linkIcon = document.createElement('i');
+        linkIcon.classList.add('fas', 'fa-link');
+
+        let linkCount = document.createElement('span');
+        linkCount.innerHTML = task.linkCount;
+
+        taskLink.appendChild(linkIcon);
+        taskLink.appendChild(linkCount);
+
+        taskFooter.appendChild(taskLink);
+
+        // Task People
+        const taskPeople = document.createElement('div');
+        taskPeople.className = 'task-people';
+
+        const addIconDiv = document.createElement('div');
+        const addIcon = document.createElement('i');
+        addIcon.classList.add('fas', 'fa-plus');
+        addIconDiv.appendChild(addIcon);
+
+        taskPeople.appendChild(addIconDiv);
+
+        const peopleIconDiv = document.createElement('div');
+        const peopleIcon = document.createElement('i');
+        peopleIcon.classList.add('fas', 'fa-user');
+        peopleIconDiv.appendChild(peopleIcon);
+
+        taskPeople.appendChild(peopleIconDiv);
+
+        taskFooter.appendChild(taskPeople);
+
+        taskCard.appendChild(taskFooter);
+
+        taskListCard.appendChild(taskCard);
+
+    })
+
+    // Add task button
+    const taskAddDiv = document.createElement('div');
+    taskAddDiv.className = 'task-add';
+    taskAddText = document.createElement('span');
+    taskAddText.innerHTML = 'Add Task';
+    taskAddDiv.appendChild(taskAddText);
+
+    taskAddIcon = document.createElement('i');
+    taskAddIcon.classList.add('fas', 'fa-plus');
+    taskAddDiv.appendChild(taskAddIcon);
+
+    taskListCard.appendChild(taskAddDiv);
+}
+
+
+const main = (boardsData) => {
+
+    createCards(boardsData);
+
+    // boardContainer.appendChild(taskListHtml);
+
+}
+main(boards);
 
